56a00d4bbcd775458f99368a9c02b79df02be8be
emhedlin <hedlin@ualberta.ca>
2021-01-20 21:33:08 -0500

    adjusted n_individuals figure

--- scripts/clean.R
+++ scripts/clean.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(nuwcru)
 
 
 dat <- read_csv("data/resight.csv")
@@ -37,6 +37,7 @@ for ( i in 1:length(tuvu_ex_list)){
   tuvu_ex_list[[i]] <- filter(tuvu_ex_list[[i]], age_est >= 0)
 }
 
+
 tuvu <- bind_rows(tuvu_ex_list) %>% 
   select(tag:origin, dead:n, "age" = "age_est") %>%
   mutate(n = ifelse(is.na(n), 0, n)) %>%
@@ -44,5 +45,49 @@ tuvu <- bind_rows(tuvu_ex_list) %>%
   arrange(tag, year) %>%
   filter(!is.na(year))
 
-tuvu
+tagged <- tuvu %>% 
+  group_by(year) %>%
+  filter(origin == 'y') %>%
+  summarize(n_tagged = sum(n, na.rm = TRUE)) 
+
+resight <- tuvu %>% 
+  group_by(year) %>%
+  filter(origin == 'n') %>%
+  summarize(n_tagged = sum(n, na.rm = TRUE)) 
+min(resight$year)
+
+# saved as jpg, 1500x383
+ggplot() +
+  geom_line(data = resight, aes(x = year, y = n_tagged), colour = grey7) +
+  geom_point(data = resight, aes(x = year, y = n_tagged), size = 5.5, shape = 21, colour = grey7, fill = "white") +
+  geom_line(data = tagged, aes(x = year, y = n_tagged)) +
+  geom_point(data = tagged, aes(x = year, y = n_tagged), size = 4, shape = 21, colour = grey2, fill = grey2) +
+  xlab("") + ylab(expression(paste(italic("n")))) +
+  scale_x_continuous(breaks = seq(2003, 2019, 1)) +
+  scale_y_continuous(breaks = seq(0, 90, 10)) +
+  geom_text(aes(x = 2016.7, y = 10), label = "tagged", family = "Times New Roman", size = 6) +
+  geom_text(aes(x = 2019, y = 10), label = "resight", family = "Times New Roman", colour = grey6, size = 6) +
+  nuwcru::theme_nuwcru() +
+  theme(axis.text.x = element_text(size =12),
+        axis.text.y = element_text(size =12),
+        axis.title.y = element_text(size = 13))
+
+age <- tuvu %>% 
+  filter(origin == 'n') %>%
+  group_by(year, age) %>%
+  tally()
+unique(age$age)
+max(age$age)
+ggplot() +
+ geom_segment(data = filter(age, age == 1), aes(x = year-0.49, xend = year-0.49, y = 0, yend = n), colour = grey7) +
+ geom_text(data = filter(age, age == 1), aes(x = year-0.43, y = n+0.5), label = "1", size = 2) +
+ geom_segment(data = filter(age, age == 2), aes(x = year-0.43, xend = year-0.43, y = 0, yend = n), colour = grey7) +
+ # geom_point(data = age, aes(x = year, y = n, group = age), colour= "white", size = 3) +
+ # geom_point(data = age, aes(x = year, y = n, group = age), colour = "white", size = 3) +
+ # geom_text(data = age, aes(x = year, y = n, group = age, label = age), size = 2.5, position=position_jitter(width=.3)) +
+  xlab("") + ylab(expression(paste(italic("n")))) +
+  scale_x_continuous(breaks = seq(2003, 2019, 1)) +
+  scale_y_continuous(breaks = seq(0, 20, 2)) +
+  theme_nuwcru()
+
 
